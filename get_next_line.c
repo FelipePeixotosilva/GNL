5d10bbb90cd904134ae04cb02a11caa23207a6f8
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-11 18:45:20 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 18:14:56 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/11 18:42:08 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
 char *get_next_line(int fd)
 {
+  char *buffer;
   
+  read(fd, buffer, 5);
+  
+  return buffer;
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd;
+    char *buffer;
+
+    fd =  open("test.txt",O_RDONLY);
+    
+    //read(fd, buffer, 5);
+    
+    printf("%s",get_next_line(fd));
 }
