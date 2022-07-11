f459fa529628805e7b539eeedd1f5a7f89a8827e
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-11 20:28:14 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 18:47:41 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/11 19:28:09 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
 
