0737135409420beba1c0c2160e4ea964096406f4
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-15 10:49:33 -0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,30 +6,23 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 08:39:26 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/14 20:47:25 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/15 10:47:39 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
 # include <stdlib.h>
 # include <unistd.h>
 
-
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
-char *get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char  *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strchr(const char *s, int c);
-
-
-
+int		sch_limit(const char *s, int c);
 
 #endif
