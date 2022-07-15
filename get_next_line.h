edb4660dab6cc9b1d5561e3654eac5130b704e56
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-14 22:55:16 -0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 08:39:26 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/11 19:34:16 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/14 20:47:25 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,19 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFFER_SIZE 15
 
 size_t	ft_strlen(const char *s);
 char *get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char  *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+
+
+
 
 #endif
