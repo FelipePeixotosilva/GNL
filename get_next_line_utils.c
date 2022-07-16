ad88eac6f73f83e9ec0d39a6ae2f2a2a8eb348e9
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-16 19:01:14 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 18:47:41 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/15 10:36:12 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/16 17:54:49 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,29 @@
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*ptr;
-	size_t	s;
-
-	if (!s1)
-	{
-		s1 = (char *)malloc(1 * sizeof(char));
-		s1[0] = '\0';
-	}
-	s = (ft_strlen(s1) + ft_strlen(s2)) + 1;
-	ptr = malloc(sizeof(char) * s);
+	//size_t	s;
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	j = 0; 
+	
+	//s = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (ptr == 0)
 	{
 		return (NULL);
 	}
-	ft_strlcpy (ptr, s1, s);
-	ft_strlcat (ptr, s2, s);
+	while(s1[i])
+	{
+		ptr[i] = s1[i];
+		i++; 
+	}
+	while(s2[j])
+		ptr[i++] = s2[j++];
+	ptr[i] = '\0';
 	free(s1);
 	return (ptr);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	dst_len;
-	size_t	j;
-	size_t	limit;
-
-	dst_len = ft_strlen(dst);
-	if (size <= dst_len)
-	{
-		return (size + ft_strlen(src));
-	}
-	limit = size - dst_len - 1;
-	j = 0;
-	while (src[j] && j < limit)
-	{
-		dst[dst_len + j] = src[j];
-		j++;
-	}
-	dst[dst_len + j] = '\0';
-	return (dst_len + ft_strlen(src));
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	if (ft_strlen(src) + 1 < size)
-	{
-		ft_memmove(dst, src, ft_strlen(src) + 1);
-		dst[size - 1] = '\0';
-	}
-	else if (size != 0)
-	{
-		ft_memmove(dst, src, size - 1);
-		dst[size - 1] = '\0';
-	}	
-	return (ft_strlen(src));
 }
 
 size_t	ft_strlen(const char *s)
@@ -77,33 +46,6 @@ size_t	ft_strlen(const char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
-}
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (dest > src)
-	{
-		while (n > 0)
-		{
-			n--;
-			((char *)dest)[n] = ((char *)src)[n];
-		}
-	}
-	else
-	{
-		while (n > 0)
-		{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
-		n--;
-		}
-	}
-	return (dest);
 }
