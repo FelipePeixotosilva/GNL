271f53e40312b8f23cdee41d5a3ba27f58d9777d
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-18 17:32:10 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,103 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/11 18:14:56 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/17 10:50:43 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/07/11 18:47:41 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/17 10:56:34 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_get_new_line(char *str)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int		i;
-	int		j;
-	char	*temp;
+	char	*ptr;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	if (!str[i])
-	{
-		free(str);
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (ptr == 0)
 		return (NULL);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
 	}
-	temp = malloc((ft_strlen(str) - i) + 1);
-	while (str[i])
-		temp[j++] = str[i++];
-	temp[j] = '\0';
-	free(str);
-	return (temp);
+	while (s2[j])
+		ptr[i++] = s2[j++];
+	ptr[i] = '\0';
+	free(s1);
+	return (ptr);
 }
 
-char	*ft_get_line(char *str)
+size_t	ft_strlen(const char *s)
 {
-	char	*temp;
-	int		i;
+	size_t	i;
 
 	i = 0;
-	if (!str[i])
-		return (NULL);
-	while (str[i] && str[i] != '\n')
+	while (s[i] != '\0')
 		i++;
-	temp = malloc (i + 2);
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
 	i = 0;
-	while (str[i] && str[i] != '\n')
+	while (i <= ft_strlen(s))
 	{
-		temp[i] = str[i];
+		if (s[i] == (char)c)
+		{
+			return ((char *)&s[i]);
+		}
 		i++;
 	}
-	if (str[i] == '\n')
-		temp[i++] = '\n';
-	temp[i] = '\0';
-	return (temp);
-}
-
-char	*ft_read_line(int fd, char *str, char *buff)
-{
-	int	rd;
-
-	rd = 1;
-	while (rd != 0)
-	{
-		rd = read (fd, buff, BUFFER_SIZE);
-		if (rd == -1)
-		{
-			free(buff);
-			return (NULL);
-		}
-		buff[rd] = '\0';
-		if (!str)
-		{
-			str = malloc(1);
-			str[0] = '\0';
-		}
-		str = ft_strjoin (str, buff);
-		if (ft_strchr(buff, '\n'))
-			break ;
-	}
-	return (str);
-}
-
-char	*get_next_line(int fd)
-{
-	char		*buff;
-	char		*line;
-	static char	*str;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read (fd, 0, 0) < 0)
-		return (NULL);
-	buff = malloc (BUFFER_SIZE + 1);
-	str = ft_read_line(fd, str, buff);
-	free(buff);
-	line = ft_get_line (str);
-	str = ft_get_new_line (str);
-	return (line);
+	return (NULL);
 }
