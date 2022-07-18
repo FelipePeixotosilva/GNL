031a7dca6b39998cda8f719c7c56237bb1315f60
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-18 17:27:44 -0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -16,12 +16,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 
